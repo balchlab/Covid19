d8542283d73d7f842339e45a1babf7a703a367c0
balchlab <dshak@scripps.edu>
2021-01-19 14:27:03 -0800

    Added scripts for nsp7, 8, pol, spike

--- Pymol/scripts/color_atoms_nsp7.py
+++ Pymol/scripts/color_atoms_nsp7.py
@@ -13,12 +13,12 @@ import camera_angles
 path = '../gpr_values/minvar_7_1_20/minvar_7_1_20'
 delta = timedelta(days=1)
 cov2_structures = pd.read_excel('../CoV2_structures_ITASSER.xlsx',usecols=[2,3])
-structure = '6vxx.pdb'
+structure = 'QHD43415_7.pdb'
 
 #Index of single protein to use from excel file
-val=15
+val=6
 
-colored_path = f"../colored_proteins/{cov2_structures.loc[val,'name']}_6vxx_{date.today().strftime('%m_%d')}"
+colored_path = f"../colored_proteins/{cov2_structures.loc[val,'name']}{date.today().strftime('%m_%d')}"
 if not os.path.exists(colored_path):
 	os.mkdir(colored_path)
 
@@ -33,18 +33,15 @@ while start_date <= end_date:
 	cmd.iterate('All_CA',"cmd.set_color(f'{num2words(resi)}_{name}',list(np.floor(gpr_vals.loc[resv-1,['R','G','B']]*255).astype(int)))")
 	cmd.alter('All_CA',"color = cmd.get_color_index(f'{num2words(resi)}_{name}')")
 
-	#Set camera angle from saved matrix
-	cmd.set_view (camera_angles.m6vxx)
 	cmd.bg_color('white')
 	cmd.pseudoatom('forlabel',label=f"{start_date.strftime('%m_%d_%y')}")
 	cmd.set('label_relative_mode',1)
 	cmd.set('label_screen_point',[-0.75,-0.8,1])
 	cmd.set('label_size',25)
-	cmd.hide('licorice')
 	cmd.deselect()
 
 # 	#Fix below for correct path
-	cmd.png(f"{colored_path}/6vxx_{cov2_structures.loc[val,'name']}_{start_date.strftime('%m_%d_%y')}.png",
+	cmd.png(f"{colored_path}/{cov2_structures.loc[val,'name']}_{start_date.strftime('%m_%d_%y')}.png",
 		width=1200, height=1200, dpi=1000, ray=1)
 	cmd.delete('all')
 	start_date += delta
